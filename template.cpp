a13c36212ba3ac98a4c5370de72e13c51fa0d632
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-02-23 01:59:43 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,40 @@
+/*
+------------------------------------------------------------------------------------------------------------------------
+You are the one that is responsible for who you are!                                                                   +
+6-Months of FOCUS & HARDWORK can put you 5-YEARS ahead in life. Don't Underestimate the power of CONSISTENCY & DESIRE  +
+------------------------------------------------------------------------------------------------------------------------
+
+***********************
+ Mehul Thuletiya      *
+ CF: Mehul_Thuletiya  *
+ CC: mehul_237        *
+***********************
+
+*/
+
+ #include <bits/stdc++.h>
+ using namespace std;
+
+ #define ll long long int 
+ #define nline "\n"
+
+ void solve() {
+     
+ } 
+
+
+ int main()
+ {
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int t; cin>>t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+
+}
