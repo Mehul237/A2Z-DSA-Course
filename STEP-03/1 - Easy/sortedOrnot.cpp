4c5bc9ecb129768006e7f7a0f0e72a65fd33cc2b
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2023-04-24 14:23:59 +0530

    03 - Check if an Array is Sorted
    Very Straight forward question
    
    Leetcode question is More Insightful!  (A2z DSA Couse - STEP-03 - 03)

---
+++ STEP-03/1 - Easy/sortedOrnot.cpp
@@ -0,0 +1,12 @@
+int isSorted(int n, vector<int> a) {
+    for(int i=0; i<n; i++) {
+        if (a[i] >= a[i-1]) {
+
+        } 
+        else {
+          return false;
+        }
+    }
+    return true;
+}
+
